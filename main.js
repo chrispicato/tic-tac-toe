43b29eaeef85e6d9d7fe2a786249d30891f74fc7
Chris Picato <christopherpicato@gmail.com>
2016-10-18 12:15:53 -0700

    Change program structure Move prompt to bottom

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 var prompt = require('prompt');
 
-prompt.start();
-
-  //
-  // Get two properties from the user: username and email
-  //
-
-var username1, marker1, username2, marker2;
-
-prompt.get(['username1', 'marker1', 'username2', 'marker2'], function (err, result) {
-  username1 = result.username1;
-  marker1 = result.market1;
-  username2 = result.username2;
-  marker2 = result.marker2;
-
-  console.log('Your results and are markers are:');
-  console.log('Player 1\'s username is: ' + result.username1);
-  console.log('Player 1\'s marker is: ' + result.marker1);
-  console.log('Player 2\'s username is: ' + result.username2);
-  console.log('Player 2\'s marker is: ' + result.marker2);
-});
-
-
-
 var board = {
   1: ' ',
   2: ' ',
@@ -37,18 +14,41 @@ var board = {
 
 var printBoard = function () {
   console.log('\n' +
-    ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3], ' |\n' +
-    ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6], ' |\n' +
-    ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9], ' |'
+    '1' + board[1] + ' |2' + board[2] + ' |3' + board[3], ' |\n' +
+    '4' + board[4] + ' |5' + board[5] + ' |6' + board[6], ' |\n' +
+    '7' + board[7] + ' |8' + board[8] + ' |9' + board[9], ' |'
   );
 };
 
+var username1, marker1, username2, marker2;
+prompt.start();
+prompt.get(['username1', 'marker1', 'username2', 'marker2', 'player1Input'], function (err, result) {
+  username1 = result.username1;
+  marker1 = result.market1;
+  username2 = result.username2;
+  marker2 = result.marker2;
+
+  console.log('Your results and are markers are:');
+  console.log('Player 1\'s username is: ' + result.username1);
+  console.log('Player 1\'s marker is: ' + result.marker1);
+  console.log('Player 2\'s username is: ' + result.username2);
+  console.log('Player 2\'s marker is: ' + result.marker2);
+
+  console.log(result.player1Input);
+  printBoard()
+});
+
 // printBoard();
 
+var player1Input;
 
-
+prompt.start();
 // create function that accepts user input
-
+prompt.get(['player1Input'], function (err, result) {
+  console.log(result.player1Input);
+  // board[result.player1Input] = marker1;
+  // printBoard();
+});
 
 // create function to validate user input
 
